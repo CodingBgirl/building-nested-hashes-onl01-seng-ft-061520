b92691cdcf33b5a032830428c7f7cc54d94c232f
Ibiene LongJohn <bgirlapollo@gmail.com>
2020-06-28 22:12:39 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,7 +3,7 @@ def third_challenge
    :montague => {
       :patriarch => {
         #your key/value pairs here
-        {ame: "Lord Montague", age: "53"} 
+        {ame: "Lord Montague", age: "53"}
         },
       :matriarch => {
         #your key/value pairs here
@@ -14,7 +14,7 @@ def third_challenge
         {name: "Romeo", age: "15", status: "alive"}
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
@@ -32,5 +32,5 @@ def third_challenge
    }
   }
 
-  
+
 end
